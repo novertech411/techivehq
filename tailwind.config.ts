e414c156e0d3b62237c3da6d423fb00d854ebfec
novertech411 <novertech4@gmail.com>
2024-06-21 12:10:31 +0100

    workrd on the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      coloros: {
+        primary: "#0D5B4B",
+        secondary: "#FCB64E",
+        lightgreen: "#18B99B",
+      },
     },
   },
   plugins: [],
