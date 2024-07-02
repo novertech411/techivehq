fbea620125649bf9bc98fd0dd4d27dca76852a73
novertech411 <novertech4@gmail.com>
2024-07-02 23:55:52 +0100

    we good

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,48 +1,53 @@
-"use Client ";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import React from "react";
 import logo from "@/public/images/logo.png";
 import Link from "next/link";
+import Container from "@/components/Container";
 
 function Navbar() {
   const pathname = usePathname();
   return (
     <div>
-      <Image src={logo} alt="techive" />
+      <Container>
+        <div className=" flex justify-between ">
+          <Image src={logo} alt="techive" />
 
-      <ul className=" ml-auto  century font-[400] text-[13px] gap-11 items-center lg:flex      hidden">
-        <Link href="/about">
-          <li
-            className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
-              pathname === "/about" ? "border-secondary" : ""
-            }   `}
-          >
-            About Us
-          </li>
-        </Link>
+          <ul className=" ml-auto  century font-[400] text-[13px] gap-11 items-center lg:flex      hidden">
+            <Link href="/about">
+              <li
+                className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
+                  pathname === "/about" ? "border-secondary" : ""
+                }   `}
+              >
+                About Us
+              </li>
+            </Link>
 
-        <Link href="/program ">
-          <li
-            className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
-              pathname === "/program" ? "border-secondary" : ""
-            }   `}
-          >
-            Programs
-          </li>
-        </Link>
+            <Link href="/program ">
+              <li
+                className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
+                  pathname === "/program" ? "border-secondary" : ""
+                }   `}
+              >
+                Programs
+              </li>
+            </Link>
 
-        <Link href="/">
-          <li
-            className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
-              pathname === "/" ? "border-secondary" : ""
-            }   `}
-          >
-            Home
-          </li>
-        </Link>
-      </ul>
-      <div></div>
+            <Link href="/">
+              <li
+                className={` py-1  border-b-2  border-primary     hover:border-secondary  ${
+                  pathname === "/" ? "border-secondary" : ""
+                }   `}
+              >
+                Home
+              </li>
+            </Link>
+          </ul>
+
+          <div> join</div>
+        </div>
+      </Container>
     </div>
   );
 }
