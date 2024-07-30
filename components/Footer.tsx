2c9d4b9105834e205ca81954edfe06c3a1971efa
novertech411 <novertech4@gmail.com>
2024-07-30 19:54:19 +0100

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,55 @@
 import Image from "next/image";
 import React from "react";
 import happy from "@/public/images/Group 1000001946.png";
+import Container from "./Container";
+import { FiInstagram } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import logo from "@/public/images/Layer_1.png";
 
 function Footer() {
   return (
-    <div className="bg-primary">
-      <div className="flex">
-        <Image src={happy} alt="happy" />
+    <div className="bg-primary p">
+      <Container>
+        <div className="flex justify-center gap-[60px] pt-[200px] items-center ">
+          <Image src={happy} alt="happy" />
 
-        <div>
-          <h2 className=" text-primary ">Learning/Mentorship Opportunities </h2>
-          <p className="max-w-[1000px] mx-auto text-[28px] font-[400]">
-            We host trainings, seminars, and bootcamps to educate and equip
-            individuals within the community with the essential skills needed to
-            enter the tech industry.
-          </p>{" "}
-          <div> </div>
+          <div className="text-center">
+            <h2 className=" text-white text-[64px] font-[600] ">
+              Join our Community{" "}
+            </h2>
+            <p className="max-w-[1000px] mx-auto text-[28px] font-[400] text-white w-[90%]  ">
+              Learn more about us and stay up to date with our activities. Join
+              our community today!
+            </p>{" "}
+            <div className="flex gap-1 text-[18px] mt-11">
+              <input
+                placeholder=" FirstName  "
+                className=" p-[14px] bg-white"
+              />
+              <input placeholder=" Email " className=" p-[14px] bg-white" />
+              <button className="p-[14px] bg-[#18B99B] text-[30px] w-[170px] text-white ">
+                {" "}
+                Join
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </Container>
+
+      <Container>
+        <div className="flex justify-between px-11 pt-[150px]  pb-11">
+          <Image src={logo} alt="" />{" "}
+          <div className="text-white text-[20px] ">
+            <p>We are social</p>
+            <div className="flex gap-3 justify-center  text-[30px]">
+              <FiInstagram className="" />
+              <FaLinkedin />
+              <BsTwitterX />
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
